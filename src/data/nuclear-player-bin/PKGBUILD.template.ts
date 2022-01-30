7a133935029862f4e8d9e80df56c1e7f74721a6f
nukeop <nukeop@gumblert.tech>
2022-01-31 00:42:28 +0100

    Initial commit

---
+++ src/data/nuclear-player-bin/PKGBUILD.template.ts
@@ -0,0 +1,31 @@
+export default `# Maintainer: nukeop <nuclear at gumblert dot tech>
+pkgname=nuclear-player-bin
+pkgver={{pkgver}}
+repurl=https://github.com/nukeop/nuclear
+commidfull=$(git ls-remote $repurl.git -tags v$pkgver)
+commid=\${commidfull:0:6}
+pkgrel=2
+pkgdesc="A free, multiplatform music player app that streams from multiple sources."
+arch=('x86_64')
+url="http://nuclear.js.org/"
+install=nuclear-player-bin.install
+license=('GPL3')
+depends=('libnotify' 'libappindicator-gtk3' 'libxtst' 'nss' )
+source=(
+    {{deburl}}
+    nuclear.desktop
+)
+md5sums=('{{debmd5}}'
+         '{{desktopmd5}}')
+
+package()   {
+    tar xf data.tar.xz
+
+    cp --preserve=mode -r usr "\${pkgdir}"
+    cp --preserve=mode -r opt "\${pkgdir}"
+
+    find "\${pkgdir}" -type d -exec chmod 755 {} +
+
+    cp --preserve=mode \${srcdir}/nuclear.desktop \${pkgdir}/usr/share/applications
+}`;
+
---
+++ src/index.ts
@@ -0,0 +1,66 @@
+import fs from "fs";
+import path from "path";
+import { program } from "commander";
+import { Md5 } from "ts-md5/dist/md5";
+import download from "download";
+import md5File from "md5-file";
+import handlebars from "handlebars";
+
+import desktopTemplate from "./data/nuclear-player-bin/nuclear.desktop.template";
+import pkgbuildTemplate from "./data/nuclear-player-bin/PKGBUILD.template";
+
+program
+  .option(
+    "-u, --deb-url <url>",
+    "Debian package URL",
+    "https://github.com/nukeop/nuclear/releases/download/nightly/nuclear-nightly.deb"
+  )
+  .option(
+    "--deb-filename <filename>",
+    "Debian package filename",
+    "nuclear-nightly.deb"
+  )
+  .option("--temp-dir <dir>", "Temporary directory name", "temp")
+  .option(
+    "--aur-repo-path <path>",
+    "AUR package repository path",
+    "/tmp/nuclear-player-bin"
+  );
+program.parse();
+const options = program.opts();
+
+(async () => {
+  const localDebPath = path.join(options.tempDir, options.debFilename);
+
+  try {
+    await fs.promises.access(options.tempDir);
+  } catch (e) {
+    await fs.promises.mkdir(options.tempDir);
+  }
+
+  try {
+    await fs.promises.access(localDebPath, fs.constants.F_OK);
+    console.log("Debian package already exists");
+  } catch (e) {
+    console.log(`Downloading ${options.debUrl}`);
+    await download(options.debUrl, options.tempDir);
+    console.log("Downloaded");
+  }
+
+  const debHash = await md5File(localDebPath);
+  const desktopHash = await Md5.hashStr(desktopTemplate);
+
+  const now = new Date();
+  const pkgver = `${now.getFullYear()}${now.getMonth() + 1}${now.getDate()}`;
+  console.log(`Setting pkgver: ${pkgver}`);
+  console.log(`Debian package hash: ${debHash}`);
+  console.log(`Desktop hash: ${desktopHash}`);
+  const pkgbuild = handlebars.compile(pkgbuildTemplate)({
+    pkgver,
+    deburl: options.debUrl,
+    debmd5: debHash,
+    desktopmd5: desktopHash,
+  });
+
+  console.log(pkgbuild);
+})();
